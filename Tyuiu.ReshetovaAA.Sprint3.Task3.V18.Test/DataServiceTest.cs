46a5fe47c5b86415ac1667e68e617b6a50a0b8be
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-11-19 19:22:47 +0500

    Task3

--- Tyuiu.ReshetovaAA.Sprint3.Task3.V18.Test/DataServiceTest.cs
+++ Tyuiu.ReshetovaAA.Sprint3.Task3.V18.Test/DataServiceTest.cs
@@ -1,6 +1,6 @@
 ﻿using Tyuiu.ReshetovaAA.Sprint3.Task3.V18.Lib;
 
-namespace Tyuiu.ReshetovaAA.Sprint3.Task6.V18.Test
+namespace Tyuiu.ReshetovaAA.Sprint3.Task3.V18.Test
 {
     [TestClass]
     public class DataServiceTest
